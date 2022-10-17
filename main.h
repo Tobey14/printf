bd12118f5020feb0b2191df48c11018dba44caf3
Tobey14 <sureboytobi@gmail.com>
2022-10-17 14:08:12 +0100

    My first commit

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/**
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list arguments);
+} specifiers_t;
+
+/*prototypes*/
+int _printf(const char *format, ...);
+
+
+#endif
